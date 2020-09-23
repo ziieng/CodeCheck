b8d282fa847a4dac3a3b1641d652d4c2ef3d53d9
ziieng <cjengelhardt@gmail.com>
2020-09-22 19:15:30 -0700

    Initial push of site structure

---
+++ Assets/script.js
@@ -0,0 +1,13 @@
+//Splash screen for game with rules
+
+//Start button: make header visible with countdown
+
+//Do a quiz
+
+//Ask for initials
+
+//Report high score to storage
+
+//Kick to scoreboard page
+
+//function in same JS to write high scores to scoreboard
