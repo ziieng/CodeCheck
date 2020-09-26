ac3104ac27f91efa24c430417a63de39fdd82549
ziieng <cjengelhardt@gmail.com>
2020-09-26 13:18:37 -0700

    Scoreboard page functions added

---
+++ Assets/scoreboard.js
@@ -0,0 +1,40 @@
+//Declare locations, global variable for score list
+const scoreArea = document.getElementById("scoreList")
+const backBtn = document.getElementById("backBtn")
+const resetBtn = document.getElementById("resetBtn")
+let scoreList = []
+//Pull scores from storage, if they exist
+if (localStorage.getItem('scoreBank') === null) {
+    scoreArea.innerHTML = "<p>No scores saved, go make one!</p>"
+} else {
+    scoreList = JSON.parse(localStorage.getItem('scoreBank'))
+    printScores()
+}
+
+//Print existing scores to page
+function printScores() {
+    //Sort highest to lowest
+    scoreList.sort(function (a, b) {
+        return a.score - b.score;
+    })
+    //Loop to print each on the page
+    for (x of scoreList) {
+        let scoreRow = document.createElement("p")
+        scoreRow.textContent = x.score + " - " + x.name
+        scoreArea.appendChild(scoreRow)
+    }
+}
+
+//Listeners
+resetBtn.addEventListener("click", clearScores);
+backBtn.addEventListener("click", function () {
+    //Kick to quiz page
+    location.replace("index.html")
+});
+
+//Clear existing scores
+function clearScores() {
+    scoreList = ""
+    localStorage.removeItem('scoreBank')
+    scoreArea.innerHTML = "<p>No scores saved, go make one!</p>"
+}
