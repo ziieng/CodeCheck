0e60d8b9df0de205ceb7672c9fd7d354ab750886
ziieng <cjengelhardt@gmail.com>
2020-09-26 14:15:49 -0700

    Added question printing functionality

--- Assets/script.js
+++ Assets/script.js
@@ -1,22 +1,28 @@
 //Declare locations, global variables (questions are at bottom of js)
-const stBtn = document.getElementById("startBtn")
+//title screen locations
 const titleCard = document.getElementById("titlePage")
-const qCard = document.getElementById("quizBody")
-const endCard = document.getElementById("endForm")
+const stBtn = document.getElementById("startBtn")
+//header/timer locations
 const timeText = document.getElementById("timeLeft")
 const timeBar = document.getElementById("timeProg")
+//quiz screen locations
+const qCard = document.getElementById("quizBody")
 const qText = document.getElementById("questionText")
 const rightA = document.getElementById("rezRight")
 const wrongA = document.getElementById("rezWrong")
-const scoreSend = document.getElementById("sendName")
+//end screen locations
+const endCard = document.getElementById("endForm")
+const endScore = document.getElementById("score")
 const inputName = document.getElementById("inputName")
+const scoreSend = document.getElementById("sendName")
+//global variables for this page
 let qNum = 0
 let nScore = 5
 
 //Listeners
 stBtn.addEventListener("click", startQuiz);
 scoreSend.addEventListener("click", addScore);
-qCard.addEventListener("click", parseAnswer(event));
+qCard.addEventListener("click", parseAnswer);
 
 //Start button: hide splash card, show question card, start timer
 function startQuiz() {
@@ -48,12 +54,50 @@ function startClock() {
 function quizCards() {
     //If this is the first question, shuffle question array
     if (qNum == 0) {
-        shuffle(qList)
+        // shuffle(qList)
+        console.log(qList)
     }
     //Display question
-
+    qText.textContent = qList[qNum].question
+    //Get object of question's answers
+    let qAnswers = qList[qNum].answers
+    qAnswers = Object.entries(qAnswers)
+    for (prop of qAnswers) {
+        console.log(prop)
+        let ansOption = document.createElement("button")
+        ansOption.setAttribute("class", "btn btn-info my-1 d-block px-3")
+        ansOption.setAttribute("ans", prop[0])
+        ansOption.textContent = prop[1]
+        qCard.appendChild(ansOption)
+    }
     //Increment question number
     qNum++
+}
+
+//Shuffle array function from https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+function shuffle(array) {
+    var currentIndex = array.length,
+        temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+//Check answer, call to show next question
+function parseAnswer() {
+
 }
 
 //Ask for initials
@@ -61,6 +105,7 @@ function gameOver() {
     qCard.classList.add("d-none")
     timeBar.parentElement.classList.add("d-none")
     endCard.classList.remove("d-none")
+    endScore.value = nScore
 }
 //Report high score to storage
 function addScore() {
@@ -82,7 +127,7 @@ function addScore() {
     location.replace("scoreboard.html")
 }
 //Question Array!
-const qList = [{
+var qList = [{
         question: "Okay but does it work tho",
         answers: {
             a: "Yes",
@@ -103,23 +148,3 @@ const qList = [{
         correctAnswer: "c"
     },
 ]
-//Shuffle array function from https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
-function shuffle(array) {
-    var currentIndex = array.length,
-        temporaryValue, randomIndex;
-
-    // While there remain elements to shuffle...
-    while (0 !== currentIndex) {
-
-        // Pick a remaining element...
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-
-        // And swap it with the current element.
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-
-    return array;
-}
