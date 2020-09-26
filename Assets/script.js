c9d81e940dcf84299908e6eaf58c15d9c0c19c64
ziieng <cjengelhardt@gmail.com>
2020-09-26 16:45:12 -0700

    added answer parsing, changed result display

--- Assets/script.js
+++ Assets/script.js
@@ -8,8 +8,9 @@ const timeBar = document.getElementById("timeProg")
 //quiz screen locations
 const qCard = document.getElementById("quizBody")
 const qText = document.getElementById("questionText")
-const rightA = document.getElementById("rezRight")
-const wrongA = document.getElementById("rezWrong")
+const qChoices = document.getElementById("answerZone")
+const resArea = document.getElementById("quizResult")
+const resText = document.getElementById("result")
 //end screen locations
 const endCard = document.getElementById("endForm")
 const endScore = document.getElementById("score")
@@ -17,35 +18,40 @@ const inputName = document.getElementById("inputName")
 const scoreSend = document.getElementById("sendName")
 //global variables for this page
 let qNum = 0
-let nScore = 5
+let nScore = 1
+let time = 75
+let xFade = 1
 
 //Listeners
 stBtn.addEventListener("click", startQuiz);
 scoreSend.addEventListener("click", addScore);
-qCard.addEventListener("click", parseAnswer);
+qChoices.addEventListener("click", function () {
+    var element = event.target
+    parseAnswer(element)
+});
 
 //Start button: hide splash card, show question card, start timer
 function startQuiz() {
-    event.preventDefault()
     titleCard.classList.add("d-none")
-    timeBar.parentElement.classList.remove("d-none")
+    timeBar.parentElement.classList.remove("invisible")
     qCard.classList.remove("d-none")
+    resArea.classList.add("invisible")
     startClock()
     quizCards()
 }
 
 //Function to start timer, holds interval for countdown
 function startClock() {
-    let t = 75
     let tPercent = 100
     let countdown = setInterval(function () {
-        t--
-        timeText.innerHTML = t
-        tPercent = t / 75 * 100
-        timeBar.setAttribute("aria-valuenow", t)
+        time--
+        timeText.innerHTML = time
+        tPercent = time / 75 * 100
+        timeBar.setAttribute("aria-valuenow", time)
         timeBar.style = "width:" + tPercent + "%"
-        if (t <= 0) {
+        if (time <= 0) {
             clearInterval(countdown)
+            timeText.innerHTML = "0"
             gameOver()
         }
     }, 1000)
@@ -54,8 +60,7 @@ function startClock() {
 function quizCards() {
     //If this is the first question, shuffle question array
     if (qNum == 0) {
-        // shuffle(qList)
-        console.log(qList)
+        shuffle(qList)
     }
     //Display question
     qText.textContent = qList[qNum].question
@@ -63,15 +68,12 @@ function quizCards() {
     let qAnswers = qList[qNum].answers
     qAnswers = Object.entries(qAnswers)
     for (prop of qAnswers) {
-        console.log(prop)
         let ansOption = document.createElement("button")
         ansOption.setAttribute("class", "btn btn-info my-1 d-block px-3")
         ansOption.setAttribute("ans", prop[0])
         ansOption.textContent = prop[1]
-        qCard.appendChild(ansOption)
+        qChoices.appendChild(ansOption)
     }
-    //Increment question number
-    qNum++
 }
 
 //Shuffle array function from https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
@@ -91,19 +93,50 @@ function shuffle(array) {
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
     }
-
     return array;
 }
 
 //Check answer, call to show next question
-function parseAnswer() {
+function parseAnswer(element) {
+    let ansPick = element.getAttribute("ans")
+    console.log(ansPick)
+    if (ansPick == qList[qNum].correctAnswer) {
+        nScore += 3
+        resArea.classList.remove("invisible")
+        resText.textContent = "Correct!"
+        fadeHandler()
+    } else {
+        time -= 6
+        resArea.classList.remove("invisible")
+        resText.textContent = "Wrong!"
+        fadeHandler()
+    }
+    qChoices.innerHTML = ""
+    //Increment question number
+    qNum++
+    //If we're out of questions, start over
+    if (qList[qNum] == null) {
+        qNum = 0
+    }
+    quizCards()
+}
 
+//Utility function to handle fading the right/wrong indicator
+function fadeHandler() {
+    xFade = 1
+    let fTime = setInterval(function () {
+        if (xFade <= 0) {
+            resArea.classList.add("invisible")
+            clearInterval(fTime)
+        }
+        xFade--
+    }, 500)
 }
 
 //Ask for initials
 function gameOver() {
     qCard.classList.add("d-none")
-    timeBar.parentElement.classList.add("d-none")
+    timeBar.parentElement.classList.add("invisible")
     endCard.classList.remove("d-none")
     endScore.value = nScore
 }
@@ -114,7 +147,7 @@ function addScore() {
         localStorage.setItem('scoreBank', '{}');
         var scoreList = []
     } else {
-        var scoreList = JSON.parse(localStorage.getItem(scoreBank))
+        var scoreList = JSON.parse(localStorage.getItem('scoreBank'))
     }
     let newScore = {
         score: nScore,
